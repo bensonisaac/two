2a96720691e0207bb653d1d02c024ec7581c1b70
bensonisaac <oluchukwubensonisaac@gmail.com>
2023-09-11 13:42:53 +0100

    Update settings.py

--- two/two/settings.py
+++ two/two/settings.py
@@ -71,13 +71,13 @@ WSGI_APPLICATION = 'two.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 DATABASES = {
